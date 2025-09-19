c417c67afb169f9b1e6899cc1dc5e065d94b54df
SruthiPuli <sruthipulipati249@gmail.com>
2025-09-19 21:54:44 +0530

    Created Django Backend

--- Django_Backend/Django_Backend/settings.py
+++ Django_Backend/Django_Backend/settings.py
@@ -27,13 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'growwellhospitals@gmail.com'  
-EMAIL_HOST_PASSWORD = 'birartngdxvajsvr'        
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 # Application definition
 
 INSTALLED_APPS = [
