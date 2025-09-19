c915e249e2630f53e6ea9ddbeb40472d32674303
SruthiPuli <sruthipulipati249@gmail.com>
2025-09-19 18:29:58 +0530

    Created Django Backend

--- Django_Backend/StaticReactJSFrontend/urls.py
+++ Django_Backend/StaticReactJSFrontend/urls.py
@@ -1,9 +1,9 @@
 from django.urls import re_path
-from .views import FrontendAppView
+from .views import ReactAppView
 
 urlpatterns = [
     # Your API endpoints here
 
     # Catch all other routes and serve React app
-    re_path(r'^.*$', FrontendAppView.as_view(), name='frontend'),
+    re_path(r'^.*$', ReactAppView.as_view(), name='frontend'),
 ]
